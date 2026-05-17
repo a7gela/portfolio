af0453c40565c3baae7f18ead75c22d209d48088
Angela Ho <angelah1324@gmail.com>
2026-05-18 00:46:41 +1000

    fixing image issues

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,32 +1,28 @@
-export const projects = [
-  {
-    title: "Vocab Forest: Web App",
-    subtitle: "Full stack development, UI/UX",
-    description: "A web app that allows users to search and save words/definitions into lists. Built using React and Supabase, and styled using Tailwind.",
-    tags: ["React", "Tailwind", "Supabase", "TypeScript", "Cloudflare"],
-    links: [
-      { name: "Live Site", link: "https://vocabforest.com/" },
-      { name: "View Repo", link: "https://github.com/a7gela/vocab-forest/" },
-    ],
-    linkId: "vocabForestLinks",
-    image: {source: "../src/assets/vocab-forest/vocab-forest-homepage.webp", alt: "Screenshot of Vocab Forest's homepage"},
-    cardId: "vocab-forest-card",
-  },
-  {
-    title: "Data Visualisations",
-    subtitle: "Data visualisation, Data cleaning and analysis",
-    description: "Interactive data visualisations exploring Australian diets (Tableau) and shark bite incidents (VegaLite, JavaScript) that I created during uni.",
-    tags: ["Tableau", "VegaLite", "JavaScript", "HTML/CSS"],
-    links:
-      [
-        { name: "Australian Diet", link: "https://public.tableau.com/app/profile/angela1624/viz/AusDiet/Dashboard3" },
-        { name: "Shark bite incidents", link: "https://a4gela.github.io/3179_A2/" },
-      ],
-    linkId: "dataVisLinks",
-    image: {source: "../src/assets/data-vis/data-vis-card-image.webp", alt: "Screenshot of Australian diet visualisation and Shark bite incidents visualisation"},
-    cardId: "data-vis-card"
-  }
-]
+export const vocabForest = {
+  title: "Vocab Forest: Web App",
+  subtitle: "Full stack development, UI/UX",
+  description: "A web app that allows users to search and save words/definitions into lists. Built using React and Supabase, and styled using Tailwind.",
+  tags: ["React", "Tailwind", "Supabase", "TypeScript", "Cloudflare"],
+  links: [
+    { name: "Live Site", link: "https://vocabforest.com/" },
+    { name: "View Repo", link: "https://github.com/a7gela/vocab-forest/" },
+  ],
+  linkId: "vocabForestLinks",
+  imageAlt: "Screenshot of Vocab Forest's homepage",
+  cardId: "vocab-forest-card",
+}
 
-export const vocabForest = projects[0];
-export const dataVis = projects[1];
+export const dataVis = {
+  title: "Data Visualisations",
+  subtitle: "Data visualisation, Data cleaning and analysis",
+  description: "Interactive data visualisations exploring Australian diets (Tableau) and shark bite incidents (VegaLite, JavaScript) that I created during uni.",
+  tags: ["Tableau", "VegaLite", "JavaScript", "HTML/CSS"],
+  links:
+    [
+      { name: "Australian Diet", link: "https://public.tableau.com/app/profile/angela1624/viz/AusDiet/Dashboard3" },
+      { name: "Shark bite incidents", link: "https://a4gela.github.io/3179_A2/" },
+    ],
+  linkId: "dataVisLinks",
+  imageAlt: "Screenshot of Australian diet visualisation and Shark bite incidents visualisation",
+  cardId: "data-vis-card"
+}
